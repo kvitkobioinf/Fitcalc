27c62436a7e944eebfe39669fdff926dbab8e5a3
Daria <boginskad@gmail.com>
2020-06-02 18:22:16 +0200

    ekran główny

--- app/src/main/java/pl/fitcalc/fitcalc/MainActivity.java
+++ app/src/main/java/pl/fitcalc/fitcalc/MainActivity.java
@@ -15,29 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button przycisk = findViewById(R.id.policz);
-        przycisk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int wzrost;
-                float waga;
-                float BMI;
-
-                wzrost = Integer.parseInt(String.valueOf(((EditText) findViewById(R.id.wzrost)).getText()));
-
-                waga = Float.parseFloat(String.valueOf(((EditText) findViewById(R.id.waga)).getText()));
-
-                float wzrost_w_metrach = wzrost / 100f;
-
-                BMI = waga / (wzrost_w_metrach * wzrost_w_metrach);
-
-                TextView miejsce_dla_wyniku = findViewById(R.id.pole_tekstowe);
-                miejsce_dla_wyniku.setText("Twój BMI to " + String.valueOf(BMI));
-            }
-
-        });
     }
 
 }
